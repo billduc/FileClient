8f88960783a2e9af0a025783446ba40f17d4075e
hydra <nguyentrungduc08@gmail.com>
2017-12-29 15:14:50 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: hydra
+ *
+ * Created on December 28, 2017, 3:55 PM
+ */
+
+#include <cstdlib>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/fcntl.h>
+#include <sys/param.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int socket;
+    
+    //check args
+    
+    
+    return 0;
+}
+
