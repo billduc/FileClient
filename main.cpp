0671d661dcecfe7bfea3de6949bb50b7b0f149ba
hydra <nguyentrungduc08@gmail.com>
2017-12-29 18:10:10 +0700

    update to test

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
  *
  * Created on December 28, 2017, 3:55 PM
  */
-
+#include <iostream>
 #include <cstdlib>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -18,7 +18,12 @@
 #include <sys/param.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
+#include <netdb.h>
 
 using namespace std;
 
@@ -26,10 +31,65 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-    int socket;
+    int socketfd, port, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    
+    char buffer[256];
     
     //check args
+    if (argc < 3){
+        cerr << "usage: "<< argv[0] << " <hostname> <port>" << endl;
+        exit(EXIT_FAILURE);
+    }
     
+    port = atoi(argv[2]);
+    
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if (socketfd < 0){
+        cerr << "ERROR create socket" << endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(port);
+    if (connect(socketfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+        cerr << "ERROR connecting" << endl;
+    
+    printf("Please enter the message: ");
+    
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    
+    n = write(socketfd, buffer, strlen(buffer));
+    
+    string filepath = "/home/hydra/Desktop/untitled.c";
+    
+    FILE * fp = fopen(filepath.c_str(), "r");
+    fseek(fp, 0, SEEK_END);
+   
+    long FileSize = ftell(fp);
+    
+    cout << FileSize << endl;
+    
+    if (n < 0) 
+         cerr << "ERROR writing to socket" << endl;
+    bzero(buffer, 256 );
+    n = read(socketfd, buffer, 255);
+    if (n < 0) 
+         cerr << "ERROR reading from socket" << endl;
+    printf("%s\n", buffer);
+    
+    close(socketfd);
     
     return 0;
 }
